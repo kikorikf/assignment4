c14d72381e4f2bfe30646df6c39cb5653291ef79
kikorikf <96519696+kikorikf@users.noreply.github.com>
2022-02-26 19:13:10 +0600

    Add files via upload

---
+++ SalamUaleikum/ВОТ ЭТО/hit.js
@@ -0,0 +1,60 @@
+function Heart(){
+    var visi=document.querySelector(".visibility");
+    var visis=document.querySelector(".visibilitys");
+    var top = document.querySelector(".top");
+    var bottom = document.querySelector(".bottom");
+    visis.style.visibility="visible";
+    visi.style.visibility="visible";
+    bottom.value = "Aspirin to prevent blood clots" + "\n" + "\n" + "ACE inhibitors or ARBs to treat high blood pressure and other conditions" + "\n" + "\n" + "Antiarrhythmics to treat arrythmias" + "\n" + "\n" + "Anticoagulants (blood thinners) to prevent blood clots" + "\n" + "\n" + "Beta blockers to help your heart work better" + "\n" + "\n" + "Calcium channel blockers to treat high blood pressure and other conditions" + "\n" + "\n" + "Statins to lower “bad” cholesterol";
+    top.value = "Heart";
+}
+function Lungs(){
+    var visi=document.querySelector(".visibility");
+    var visis=document.querySelector(".visibilitys");
+    var top = document.querySelector(".top");
+    var bottom = document.querySelector(".bottom");
+    visis.style.visibility="visible";
+    visi.style.visibility="visible";
+    bottom.value = "Short active bronchodilators:" + "\n" + "Albuterol (Proair HFA, Ventolin HFA)" + "\n" + "Levalbuterol (Xopenex)" + "\n" + "Ipratropium (Atrovent HFA)" + "\n" + "Albuterol/ipratropium (Combivent Respimat)" + "\n" + "\n" + "Long-acting bronchodilators:" + "\n" + "Aclidinium (Tudorza)" + "\n" + "Arformoterol (Brovana)" + "\n" + "Formoterol (Foradil, Perforomist)" + "\n" + "Glycopyrrolate (Seebri Neohaler, Lonhala Magnair)" ;
+    top.value = "Lungs";
+}
+function Stomach(){
+    var visi=document.querySelector(".visibility");
+    var visis=document.querySelector(".visibilitys");
+    var top = document.querySelector(".top");
+    var bottom = document.querySelector(".bottom");
+    visis.style.visibility="visible";
+    visi.style.visibility="visible";
+    bottom.value = "For Nausea and Vomiting:" + "\n" + "Bismuth subsalicylate, the active ingredient in OTC medications like Kaopectate® and Pepto-Bismol™, protects your stomach lining. Bismuth subsalicylate is also used to treat ulcers, upset stomach and diarrhea" + "\n" + "\n" + "Other medicines include cyclizine, dimenhydrinate, diphenhydramine, and meclizine. These can be found in medicines such as Dramamine, Bonine, or others, and they dull motion sickness by acting on your brain. They block messages from reaching the part of the brain that controls nausea and vomiting" + "\n" + "\n" + "For cramping from diarrhea, medicines that have loperamide (Imodium) or bismuth subsalicylate (Kaopectate or Pepto-Bismol) might make you feel better. For other types of pain, acetaminophen (Aspirin Free Anacin, Liquiprin, Panadol, Tylenol) might be helpful";
+    top.value="Stomach";
+}
+function Headache(){
+    var visi=document.querySelector(".visibility");
+    var visis=document.querySelector(".visibilitys");
+    var top = document.querySelector(".top");
+    var bottom = document.querySelector(".bottom");
+    visis.style.visibility="visible";
+    visi.style.visibility="visible";
+    bottom.value = "Acute medications:" + "\n" + "\n" + "Pain relievers. Simple pain relievers available without a prescription are usually the first line of treatment for reducing headache pain. These include the drugs aspirin, ibuprofen (Advil, Motrin IB, others) and naproxen sodium (Aleve)." + "\n" + "\n" + "Combination medications. Aspirin or acetaminophen (Tylenol, others) or both are often combined with caffeine or a sedative drug in a single medication. Combination drugs may be more effective than single-ingredient pain relievers. Many combination drugs are available without a prescription." + "\n" + "\n" + "Triptans and narcotics. For people who experience both migraines and episodic tension-type headaches, a triptan can effectively relieve the pain of both headaches. Opioids, or narcotics, are rarely used because of their side effects and potential for dependency." + "\n" + "\n" + "Preventive medications:" + "\n" + "\n" + "Tricyclic antidepressants. Tricyclic antidepressants, including amitriptyline and protriptyline, are the most commonly used medications to prevent tension-type headaches. Side effects of these medications may include constipation, drowsiness and dry mouth." + "\n" + "\n" + "Other antidepressants. Evidence also supports the use of the antidepressants venlafaxine (Effexor XR) and mirtazapine (Remeron)";
+    top.value = "Headache";
+}
+function Muscles(){
+    var visi=document.querySelector(".visibility");
+    var visis=document.querySelector(".visibilitys");
+    var top = document.querySelector(".top");
+    var bottom = document.querySelector(".bottom");
+    visis.style.visibility="visible";
+    visi.style.visibility="visible";
+    bottom.value = "Common Muscle Relaxants:" + "\n" + "\n" + "Baclofen. Muscle tightness and muscle spasms, including those related to spine injuries, may be eased with baclofen. The medication may be helpful in treating multiple sclerosis and stabbing nerve pain. It is available as a tablet and can be taken by children as young as 12 years old." + "\n" + "\n" + "Benzodiazepines. In addition to treating anxiety, alcohol withdrawal, and seizure disorders, such as epilepsy, benzodiazepines can also treat muscle spasms and skeletal pain." + "\n" + "\n" + "Carisoprodol (Soma). Carisoprodol relaxes muscles and eases pain and stiffness caused by acute bone and muscle problems, often caused by an injury. It is taken by mouth in tablet form and is also available in combination with aspirin or aspirin and codeine." + "\n" + "\n" + "Chlorzoxazone (Lorzone). Chlorzoxazone is used for the relief of discomfort from acute, painful, musculoskeletal conditions." + "\n" + "\n" + "Dantrolene (Dantrium). Dantrolene helps control chronic spasticity related to spinal injuries. It is also used for conditions such as stroke, multiple sclerosis, and cerebral palsy." + "\n" + "\n" + "Metaxalone (Skelaxin, Metaxall, and Metaxall CP, Lorvatus PharmaPak). Metaxalone targets pain and muscle spasms from sprains, strains, and muscle injuries. It is available as a tablet or injection." ;
+    top.value="Muscles";
+}
+function Intestine(){
+    var visi=document.querySelector(".visibility");
+    var visis=document.querySelector(".visibilitys");
+    var top = document.querySelector(".top");
+    var bottom = document.querySelector(".bottom");
+    visis.style.visibility="visible";
+    visi.style.visibility="visible";
+    bottom.value = "Antacids:" + "\n" + "Aluminum hydroxide, magnesium hydroxide (Mylanta, Maalox)" + "\n" + "Calcium carbonate (Tums, Rolaids, Chooz)" + "\n" + "Bismuth subsalicylate (Pepto-Bismol)" + "\n" + "\n" + "Proton Pump Inhibitors:" + "\n" + "Omeprazole (Prilosec)" + "\n" + "Lansoprazole (Prevacid)" + "\n" + "Rabeprazole (Aciphex)" + "\n" + "Esomeprazole (Nexium)" + "\n" + "Histamine2 Blockers:" + "\n" + "Cimetidine (Tagamet)" + "\n" + "Famotidine (Pepcid)" + "\n" + "Nizatidine (Axid)";
+    top.value = "Intestlne";
+}
